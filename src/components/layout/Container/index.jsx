d8ac9b49e25b50f86bd86f489967aecc030fb6a7
wesley_da_silva <wesleyysilvaa1912@gmail.com>
2025-01-19 20:24:56 -0300

    Configuring the container

--- src/components/layout/Container/index.jsx
+++ src/components/layout/Container/index.jsx
@@ -1,9 +1,14 @@
 import styles from './Container.module.css'
+import { useTheme } from '../../../context/ThemeModeContext';
 
 const Container = ({ children }) => {
+
+  const { theme } = useTheme()
+  
   return (
-    <main className={styles.container}>
-        { children }
+    <main
+      className={`${styles.container} ${theme ? 'dark-theme' : 'light-theme'}`}>
+      { children }
     </main>
   )
 };
